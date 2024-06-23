5adfad85106b7ef3fa96f9a8eddd6fef25c11b69
Tara Walenczyk <t.walenczy@gmail.com>
2024-06-23 16:34:40 -0400

    new

--- src/app/components/Blog/RenderMdx.js
+++ src/app/components/Blog/RenderMdx.js
@@ -14,13 +14,11 @@ const RenderMdx = ({ blog }) => {
       console.log(blog);
     }, [blog]);
 
-    const MDXContent = blog && blog.body && blog.body.code ? useMDXComponent(blog.body.code) : null;
+    const MDXContent = useMDXComponent(blog.body.code);
 
     if (!blog || !blog.body || !blog.body.code) {
         return <div>Error: Blog content is not available.</div>;
       }
-
-
   return (
         <div className='col-span-8 font-in prose-lg w-max-w prose-blockquote:bg-Grey/20 prose-blockquote:p-2 prose-blockquote:px-6 prose-blockquote:border-LightPurple
         prose-blockquote:not-italic
